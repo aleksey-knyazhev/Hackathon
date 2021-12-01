9d72a4d7ee4899c5c2cdb185cfd6a9c127d95618
Alexey <AAnaKireev@sberbank.ru>
2021-12-01 22:13:03 +0300

    1. Реализованы 1 и 2 сценарии. Написан метод для 3 сценария. 2. Добавлены конфиги бота

--- src/main/kotlin/ru/RegistrationBot/RegistrationBot.kt
+++ src/main/kotlin/ru/RegistrationBot/RegistrationBot.kt
@@ -7,14 +7,18 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.objects.Update
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow
+import java.time.LocalDate
 
 @Service
-class RegistrationBot : TelegramLongPollingBot()  {
+class RegistrationBot : TelegramLongPollingBot() {
     @Value("\${telegram.botName}")
     private val botName: String = ""
 
     @Value("\${telegram.token}")
     private val token: String = ""
+
+    @Value("\${telegram.doctor.chatId}")
+    private val doctor: Long = 0
 
     override fun getBotToken(): String = token
 
@@ -24,40 +28,97 @@ class RegistrationBot : TelegramLongPollingBot()  {
         if (update.hasMessage()) {
             val message = update.message
             val chatId = message.chatId
+            var buttons: MutableList<String> = mutableListOf("Главное меню")
+            var date: String
+            var time: String
             val responseText = if (message.hasText()) {
                 val messageText = message.text
+                val text = if (chatId == doctor) {
+                    when {
+                        messageText == "/start" -> {
+                            buttons.add("Открыть запись")
+                            buttons.add("Показать свободное время")
+                            "Здравствуй, хозяин!"
+                        }
+                        messageText.startsWith("Открыть запись") -> "Введите дату в формате ГГГГ-ММ-ДД\nНапример: 2021-10-08"
+                        messageText.matches(Regex("\\d{4}-\\d{2}-\\d{2}")) -> {
+                            date = messageText
+                            "Введите время первой и последней записи через пробел в формате hh:mm\nНапример: 10:00 18:30"
+                        }
+                        messageText.matches(Regex("\\d{2}:\\d{2} \\d{2}:\\d{2}")) -> {
+                            time = messageText
+                            //метод, который принимает дату и время для открытия записи - создается таблица в бд
+                            // LocalDate.parse(date)
+                            "Запись открыта успешно"
+                        }
+                        messageText.startsWith("Главное меню") -> {
+                            date = ""
+                            time = ""
+                            "/start"
+                        }
+                        else -> "Вы написали: *$messageText*"
+                    }
+                } else {
+                    when {
+                        messageText == "/start" -> {
+                            buttons.add("Показать свободное время")
+                            "Добро пожаловать!"
+                        }
+                        else -> "Вы написали: *$messageText*"
+                    }
+                }
                 when {
-                    messageText == "/start" -> "Добро пожаловать!"
-                    messageText.startsWith("Кнопка ") -> "Вы нажали кнопку ${messageText.replace("Кнопка ", "")}" // обработка нажатия кнопки
-                    else -> "Вы написали: *$messageText*"
+                    messageText.startsWith("Показать свободное время") -> {
+                        //метод, возвращающий дни с открытой записью
+                        buttons.addAll(listOf("10-05-2021", "08-06-2021", "22-12-2021"))
+                        "Выберите дату"
+                    }
+                    messageText.matches(Regex("\\d{2}-\\d{2}-\\d{4}")) -> {
+                        date = messageText
+                        buttons.addAll(listOf("10:00", "15:00", "17:30"))
+                        "Выберите свободное время для записи"
+                    }
+                    messageText.matches(Regex("\\d{2}:\\d{2}")) -> {
+                        time = messageText
+                        //метод, который принимает дату и время для создания записи
+                        // LocalDate.parse(date)
+                        "Запись создана успешно"
+
+                    }
+                    else -> text
                 }
             } else {
                 "Я понимаю только текст"
             }
-            sendNotification(chatId, responseText)
+            sendNotification(chatId, responseText, buttons)
         }
     }
 
-    private fun sendNotification(chatId: Long, responseText: String) {
+    private fun sendNotification(chatId: Long, responseText: String, buttons: List<String>) {
         val responseMessage = SendMessage(chatId.toString(), responseText)
         responseMessage.enableMarkdown(true)
-        // добавляем 4 кнопки
-        responseMessage.replyMarkup = getReplyMarkup(
-            listOf(
-                listOf("Кнопка 1", "Кнопка 2"),
-                listOf("Кнопка 3", "Кнопка 4")
-            )
-        )
+        responseMessage.replyMarkup = getReplyMarkup(buttons)
+
         execute(responseMessage)
     }
 
-    private fun getReplyMarkup(allButtons: List<List<String>>): ReplyKeyboardMarkup {
+    private fun getReplyMarkup(allButtons: List<String>): ReplyKeyboardMarkup {
         val markup = ReplyKeyboardMarkup()
-        markup.keyboard = allButtons.map { rowButtons ->
+        markup.keyboard = allButtons.map { rowButton ->
             val row = KeyboardRow()
-            rowButtons.forEach { rowButton -> row.add(rowButton) }
+            row.add(rowButton)
             row
         }
         return markup
+    }
+
+    private fun requestConfirmation(chatId: Long, time: String) {
+        var buttons: List<String> = listOf(
+            "Главное меню",
+            "Подтвердить запись",
+            "Отменить запись"
+        )
+        val text = "Хотели бы вам напомнить, что завтра в $time вы записаны на прием. Подтвердите запись или отмените ее"
+        sendNotification(chatId, text, buttons)
     }
 }
