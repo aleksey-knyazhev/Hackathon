bf2e89ec16bbbf5faae49606cc18df0c77f760f2
Alexey <AAnaKireev@sberbank.ru>
2021-12-02 12:16:11 +0300

    1. Добавлена модель DTO для передачи информации о пользователе 2. Добавлены методы в сервис для работы с ботом

--- src/main/kotlin/ru/RegistrationBot/service/TelegramBotService.kt
+++ src/main/kotlin/ru/RegistrationBot/service/TelegramBotService.kt
@@ -1,5 +1,6 @@
 package ru.RegistrationBot.service
 
+import ru.RegistrationBot.dto.UserInfo
 import java.time.LocalDate
 
 interface TelegramBotService {
@@ -9,16 +10,17 @@ interface TelegramBotService {
      * date - день для записи
      * time - временной диапазон (строка вида "10:00 18:00")
      */
-    fun openNotification(date:LocalDate, time: String)
+    fun openRecording(date:LocalDate, time: String)
 
     /**
-     * Метод для открытия записи
+     * Метод для создания
      * date - день для записи
      * time - время записи (строка вида "18:00")
      * chatId - Id чата с клиентом. По нему будут отправляться оповещения
      * login - логин клиента
      * firstName - имя клиента (при наличии)
      */
+    @Deprecated(message = "использовать метод addRecording()")
     fun addNotification(date: LocalDate, time: String, chatId: Long, login: String, firstName: String?)
 
     /**
@@ -28,6 +30,53 @@ interface TelegramBotService {
 
     /**
      * Метод для получения свободных слотов времени для конкретной даты
+     * Список должен состоять из строк, содержащих id записи и время, разделенных пробелом (257 10:00)
      */
     fun getTimesForDate(date: LocalDate):List<String>
+
+    /**
+     * Метод для получения списка неподтвержденных записей на завтрашний день
+     * Список должен состоять из строк, содержащих id записи, времени записи, имя и логин клиента
+     * К логину клиента необходимо добавлять префикс @
+     */
+    fun getUnconfirmedRecording():List<String>
+
+    /**
+     * Метод для получения списка подтвержденных записей на завтрашний день
+     * Список должен состоять из строк, содержащих id записи, времени записи, имя и логин клиента
+     * К логину клиента необходимо добавлять префикс @
+     */
+    fun getConfirmedRecording():List<String>
+
+    /**
+     * Метод для создания записи
+     * idRecording - id записи
+     * user - данные о клиенте
+     */
+    fun addRecording(idRecording: Long, user: UserInfo)
+
+    /**
+     * Метод для удаления записи
+     * idRecording - id записи
+     */
+    fun deleteRecording(idRecording: Long)
+
+    /**
+     * Метод для получения списка всех пользователей
+     * idRecording - id записи
+     * в качестве элементов списка можно возвращать например UserInfo, но обязательно нужно передавать на фронт id пользователя
+     */
+    fun getAllUsers():List<String>
+
+    /**
+     * Метод для удаления пользователя
+     * idUser - id клиента
+     */
+    fun deleteUserInfo(idUser: Long)
+
+    /**
+     * Метод для получения истории по клиенту
+     * idUser - id клиента
+     */
+    fun getHistory(idUser: Long):List<String>
 }
