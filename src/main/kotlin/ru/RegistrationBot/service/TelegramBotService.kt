501d12b1737681d83381a41d90bd129faa9e77a0
Alexey <AAnaKireev@sberbank.ru>
2021-12-02 15:34:37 +0300

    Поправлены описания в JavaDoc

--- src/main/kotlin/ru/RegistrationBot/service/TelegramBotService.kt
+++ src/main/kotlin/ru/RegistrationBot/service/TelegramBotService.kt
@@ -58,6 +58,7 @@ interface TelegramBotService {
     /**
      * Метод для удаления записи
      * idRecording - id записи
+     * при удалении записи нужно оповестить клиента с помощью метода sendCancelNotification() класса RegistrationBot
      */
     fun deleteRecording(idRecording: Long)
 
@@ -71,6 +72,7 @@ interface TelegramBotService {
     /**
      * Метод для удаления пользователя
      * idUser - id клиента
+     * при удалении пользователя нужно отправить ему уведомление с помощью метода sendDeleteNotification() класса RegistrationBot
      */
     fun deleteUserInfo(idUser: Long)
 
