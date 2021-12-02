b07cca86bf7eea2ef43ee0df7f22d30bd0540666
aleksey-knyazhev <aleksey.knyazhev@gmail.com>
2021-12-02 22:49:44 +0300

    Upd

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -7,11 +7,11 @@ create table clients
         constraint clients_pk
             primary key,
     phone bigint not null,
-    "phoneString" varchar(18),
-    "telegramId" bigint not null,
-    "userName" varchar(100) not null,
-    "firstName" varchar(100),
-    "lastName" varchar(100)
+    phone_string varchar(18),
+    telegram_id bigint not null,
+    user_name varchar(100) not null,
+    first_name varchar(100),
+    last_name varchar(100)
 );
 alter table clients
     owner to postgres;
@@ -23,31 +23,31 @@ create unique index clients_phone_uindex
 
 create table schedule
 (
-    id          serial
+    id serial
         constraint schedule_pk
             primary key,
-    "timeStart" timestamp                         not null,
-    "timeEnd"   timestamp                         not null,
-    status      status default 'свободно'::status not null,
+    time_start timestamp not null,
+    time_end   timestamp not null,
+    status      status not null,
     client      integer
 );
 alter table schedule
     owner to postgres;
 create unique index schedule_id_uindex
     on schedule (id);
-create unique index schedule_timestart_uindex
-    on schedule ("timeStart");
-create unique index schedule_timeend_uindex
-    on schedule ("timeEnd");
+create unique index schedule_time_start_uindex
+    on schedule ("time_start");
+create unique index schedule_time_end_uindex
+    on schedule ("time_end");
 
 
 create table history
 (
-    id          serial
+    id serial
         constraint history_pk
             primary key,
-    clinet      integer                                         not null,
-    date        timestamp                                       not null,
+    clinet      integer not null,
+    date        timestamp not null,
     action      status default 'прочее'::status not null,
     description varchar(200)
 );
@@ -57,7 +57,7 @@ create unique index history_id_uindex
     on history (id);
 
 
-insert into clients (phone, "phoneString", "telegramId", "userName", "firstName", "lastName") values (79857197650, '+7 (901)123-12-12', 123456789, '@Pentrov', 'Петр', 'Петров');
-insert into clients (phone, "phoneString", "telegramId", "userName", "firstName", "lastName") values (2, '02', 234567890, '@Ivanov', 'Иван', 'Иванов');
-insert into schedule ("timeStart", "timeEnd", status, client) values ('2021-11-29 17:47:02.000000', '2021-11-29 17:47:04.000000', 'не подтверждено', 1);
-insert into history (clinet, date, action, description) values (1, '2021-11-30 17:06:29.000000', 'не подтверждено', 'Создал неподтвержденную запись');
+insert into clients (phone, phone_string, telegram_id, user_name, first_name, last_name) values (79857197650, '+7 (901)123-12-12', 123456789, '@Pentrov', 'Петр', 'Петров');
+insert into clients (phone, phone_string, telegram_id, user_name, first_name, last_name) values (2, '02', 234567890, '@Ivanov', 'Иван', 'Иванов');
+insert into schedule (time_start, time_end, status, client) values ('2021-11-29 17:47:02.000000', '2021-11-29 17:47:04.000000', 'не подтверждено'::status, 1);
+insert into history (clinet, date, action, description) values (1, '2021-11-30 17:06:29.000000', 'не подтверждено'::status, 'Создал неподтвержденную запись');
