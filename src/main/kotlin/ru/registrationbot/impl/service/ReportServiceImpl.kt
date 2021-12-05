bb85393e8aa498b60fdaee19e0c5ed489a3e5fcb
Olga <o2-2010@mail.ru>
2021-12-05 15:54:39 +0300

    Добавлена реализация ReportService

--- src/main/kotlin/ru/registrationbot/impl/service/ReportServiceImpl.kt
+++ src/main/kotlin/ru/registrationbot/impl/service/ReportServiceImpl.kt
@@ -2,7 +2,6 @@ package ru.registrationbot.impl.service
 
 import org.springframework.stereotype.Service
 import ru.registrationbot.api.enums.TimeslotStatus
-import ru.registrationbot.api.repository.ClientRepository
 import ru.registrationbot.api.repository.ScheduleRepository
 import ru.registrationbot.api.service.ReportService
 import ru.registrationbot.impl.entities.ClientsEntity
@@ -14,23 +13,26 @@ class ReportServiceImpl(
     private val serviceUtils: ServiceUtils
 ): ReportService {
 
-    override fun getUnconfirmedRecording(): List<String> {
+    override fun getUnconfirmedRecording() = getNotificationReportByStatus(TimeslotStatus.BOOKED)
+
+    override fun getConfirmedRecording() = getNotificationReportByStatus(TimeslotStatus.CONFIRMED)
+
+    private fun getNotificationReportByStatus(status: TimeslotStatus): List<String>  {
         val date = LocalDate.now().plusDays(1)
-//записи из расписния
-        val records = repositoryTime.findByStatusAndRecordDate(TimeslotStatus.BOOKED, date)
-//  мапа клиентов: id клиента -> объект клиента
+        //записи из расписния
+        val records = repositoryTime.findByStatusAndRecordDate(status, date)
+        //  мапа клиентов: id клиента -> объект клиента
         val clients: Map<Int, ClientsEntity> = serviceUtils.getClientsMapByRecordsTime(records)
+        val result = mutableListOf<String>()
 
-        return listOf(String())
-    }
-
-    override fun getConfirmedRecording(): List<String> {
-        val date = LocalDate.now().plusDays(1)
-//записи из расписния
-        val records = repositoryTime.findByStatusAndRecordDate(TimeslotStatus.BOOKED, date)
-//  мапа клиентов: id клиента -> объект клиента
-        val clients: Map<Int, ClientsEntity> = serviceUtils.getClientsMapByRecordsTime(records)
-
-        return listOf(String())
+        for(record in records)
+        {
+            val client = clients.get(record.client)!!
+            result.add("${record.id} ${record.recordDate} ${client.firstName.orEmpty()} " +
+                    "@${client.userName.orEmpty().replace("@","").replace("_", "\\_")} " +
+                    client.phone.orEmpty()
+            )
+        }
+        return result
     }
 }
